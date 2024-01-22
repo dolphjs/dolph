1898decf60ac4690d64f054dd603a80529878431
xlaez <utibeabasiekong2@gmail.com>
2024-01-22 02:13:31 +0100

    update 🛠️: finish manual sample test

--- samples/basic/new.controller.ts
+++ samples/basic/new.controller.ts
@@ -1,15 +1,17 @@
 import { DolphControllerHandler } from '../../classes';
 import { DRequest, DResponse, Dolph, SuccessResponse } from '../../common';
-import { Post, Route } from '../../decorators';
+import { Post, Route, UseMiddlware } from '../../decorators';
+import { testMiddleware } from './app.middleware';
 
-@Route('new')
+@Route('app')
 export class NewController extends DolphControllerHandler<Dolph> {
   constructor() {
     super();
   }
 
-  @Post('app')
+  @Post('test')
+  @UseMiddlware(testMiddleware)
   async testNewController(req: DRequest, res: DResponse) {
-    SuccessResponse({ res, body: { ...req.body } });
+    SuccessResponse({ res, body: { ...req.body, ...req.payload } });
   }
 }
