0dff316e5a421bb4375d70b38ad4772f1e448a84
Utee <utibeabasiekong2@gmail.com>
2024-08-09 16:01:27 +0100

    fix: remove argon2

--- utilities/encryptions/argon_encryption.utilities.ts
+++ utilities/encryptions/argon_encryption.utilities.ts
@@ -1,4 +1,3 @@
-import { hash, verify } from 'argon2';
 import { argonHashParam, bcryptCompareParam } from '../../common';
 
 /**
@@ -6,27 +5,27 @@ import { argonHashParam, bcryptCompareParam } from '../../common';
  * Creates a string hash using the `argon2` library
  *  - see [https://argon2.dev]
  */
-const hashWithArgon = async ({
-  pureString,
-  salt,
-  memoryCost,
-  secret,
-  version,
-  parallelism,
-  raw,
-  type,
-  timeCost,
-}: argonHashParam) => {
-  return hash(pureString, { salt, memoryCost, secret, version, parallelism, raw, type, timeCost });
-};
+// const hashWithArgon = async ({
+//   pureString,
+//   salt,
+//   memoryCost,
+//   secret,
+//   version,
+//   parallelism,
+//   raw,
+//   type,
+//   timeCost,
+// }: argonHashParam) => {
+//   return hash(pureString, { salt, memoryCost, secret, version, parallelism, raw, type, timeCost });
+// };
 
 /**
  *
  * Compare's string hash created by the argon2 library against a normal string
  *
  */
-const verifyArgonHash = async ({ pureString, hashString }: bcryptCompareParam) => {
-  return verify(hashString, pureString);
-};
+// const verifyArgonHash = async ({ pureString, hashString }: bcryptCompareParam) => {
+//   return verify(hashString, pureString);
+// };
 
-export { hashWithArgon, verifyArgonHash };
+// export { hashWithArgon, verifyArgonHash };
--- utilities/encryptions/index.ts
+++ utilities/encryptions/index.ts
@@ -1,2 +1,2 @@
-export * from './argon_encryption.utilities';
+// export * from './argon_encryption.utilities';
 export * from './bcrypt_encryption.utilities';
