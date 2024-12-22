280c5fd9a452d17be762d52310b4d2045285099f
Utee Ekong <utibeabasiekong2@gmail.com>
2024-12-22 15:09:43 +0100

    feat: add global filter exception

--- samples/demo/src/components/user/user.controller.ts
+++ samples/demo/src/components/user/user.controller.ts
@@ -1,5 +1,6 @@
+import { InternalServerErrorException } from '@dolphjs/dolph/common';
 import { DolphControllerHandler } from '../../../../../classes';
-import { Dolph, SuccessResponse, DRequest, DResponse, TryCatchAsyncDec } from '../../../../../common';
+import { Dolph, SuccessResponse, DRequest, DResponse } from '../../../../../common';
 import { Get, Post, Route } from '../../../../../decorators';
 
 @Route('user')
@@ -14,8 +15,8 @@ export class UserController extends DolphControllerHandler<Dolph> {
   }
 
   @Post('')
-  @TryCatchAsyncDec
   async post(req: DRequest, res: DResponse) {
+    throw new InternalServerErrorException('Here is an error, big time one');
     SuccessResponse({ res, body: req.body });
   }
 }
