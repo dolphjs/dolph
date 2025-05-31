3de8e52372e0534762d25b381edceb1995ab4460
utee <utibeabasiekong2@gmail.com>
2025-05-31 17:53:33 +0100

    update: bring back support for services initialising other services via components without explicitly instantiating them

--- samples/demo/src/components/user/user.service.ts
+++ samples/demo/src/components/user/user.service.ts
@@ -3,18 +3,19 @@ import { Dolph } from '../../../../../common';
 import { InjectMongo } from '../../../../../decorators';
 import { Model } from 'mongoose';
 import { UserModel, IUser } from './user.model';
+import { User2Service } from './user2.service';
 
 @InjectMongo('userModel', UserModel)
 export class UserService extends DolphServiceHandler<Dolph> {
     userModel!: Model<IUser>;
-    users: { name: string; age: number }[] = [];
+    users: { name: string; age: number; tag: string }[] = [];
 
-    constructor() {
+    constructor(private User2Service: User2Service) {
         super('userservice');
     }
 
     createUser(age: number, name: string): { name: string; age: number }[] {
-        this.users.push({ age, name });
+        this.users.push({ age, name, tag: this.User2Service.get() });
         return this.users;
     }
 }
