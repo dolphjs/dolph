01e8654c0cb9a1f73a8ac1fd3b2dd8247e9c1d65
utee <utibeabasiekong2@gmail.com>
2025-06-02 07:56:24 +0100

    feat: add param decorators for @DBody, @DQuery, @DParam, @DFile, e.t.c

---
+++ samples/demo/src/components/user/user.dto.ts
@@ -0,0 +1,15 @@
+import { Type } from 'class-transformer';
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsNumber()
+    age: number;
+
+    @Type(() => String)
+    @IsOptional()
+    gender: string;
+}
--- samples/demo/src/components/user/user2.controller.ts
+++ samples/demo/src/components/user/user2.controller.ts
@@ -1,9 +1,12 @@
 import { DolphControllerHandler } from '../../../../../classes';
-import { Dolph, DRequest, DResponse } from '../../../../../common';
-import { Get, Route } from '../../../../../decorators';
+import { Dolph, DRequest, DResponse, SuccessResponse } from '../../../../../common';
+import { DBody, DReq, DRes, Get, Post, Route } from '../../../../../decorators';
+import { CreateUserDto } from './user.dto';
+import { User2Service } from './user2.service';
 
 @Route('user2')
 export class User2Controller extends DolphControllerHandler<Dolph> {
+    private User2Service: User2Service;
     constructor() {
         super();
     }
@@ -11,5 +14,10 @@ export class User2Controller extends DolphControllerHandler<Dolph> {
     @Get('')
     get(req: DRequest, res: DResponse) {
         return req.query;
+    }
+
+    @Post('')
+    async print(@DReq() req: DRequest, @DRes() res: DResponse, @DBody(CreateUserDto) body: CreateUserDto) {
+        SuccessResponse({ res, body });
     }
 }
